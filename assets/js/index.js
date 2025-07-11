c8037f7c5320392b2d130c06e8d663bc05fbaacd
poojasharma122 <design.avology@gmail.com>
2025-07-11 12:48:57 +0530

    cahnge

--- assets/js/index.js
+++ assets/js/index.js
@@ -9,79 +9,26 @@ $(document).ready(function () {
 });
 // Toggle Menu Functionaliy End
 
-// Header Scroll JS Start
-$(document).ready(function () {
-  $(window).scroll(function () {
-    var header = $("header");
-    header.toggleClass("fixed_header", $(window).scrollTop() > 0);
-  });
-});
-// Header Scroll JS End
-
 // AOS JS Start
 AOS.init({
   duration: 1200,
 });
 // AOS JS End
 
-// Swiper JS Start
-const { Autoplay, FreeMode } = Swiper;
-Swiper.use([Autoplay, FreeMode]);
 
-const swiper = new Swiper(".swiper-container", {
-  loop: true,
-  freeMode: true,
-  speed: 3000,
-  grabCursor: true,
-  loopAdditionalSlides: 2,
-
-  autoplay: {
-    delay: 0, 
-    disableOnInteraction: false,
-    pauseOnMouseEnter: true, 
-  },
-
-  breakpoints: {
-    0: { slidesPerView: 1 },
-    480: { slidesPerView: 2 },
-    575: { slidesPerView: 3 },
-    767: { slidesPerView: 4 },
-    1024: { slidesPerView: 5 },
-    1200: { slidesPerView: 6 },
-    1400: { slidesPerView: 7 },
-  },
+// Slider JS Start
+$('.single_item_slider').slick({
+  dots: false,
+  infinite: true,
+  speed: 300,
+  autoplay: true,
+  prevArrow: `<button class="slick-prev custom-arrow custom-prev" aria-label="Previous slide">
+				 <svg  viewBox="0 0 32 72" xmlns="http://www.w3.org/2000/svg"><path stroke="#ffc723" stroke-width="1.5" d="M31 71L1 35 31 1" fill="none" fill-rule="evenodd" stroke-linecap="round" stroke-linejoin="round"></path></svg>
+				</button>`,
+  nextArrow: `<button class="slick-next custom-arrow custom-next" aria-label="Next slide">
+				 <svg  viewBox="0 0 32 72" xmlns="http://www.w3.org/2000/svg"><path stroke="#ffc723" stroke-width="1.5" d="M1 71l30-36L1 1" fill="none" fill-rule="evenodd" stroke-linecap="round" stroke-linejoin="round"></path></svg>
+				</button>`
 });
-// Swiper JS End
-
-
-
-let form = document.querySelector(".form-wrapper");
-
-if (form) {
-  form.addEventListener("submit", function(e) {
-    e.preventDefault(); 
-  });
-}
-
-// FAQ JS Start
-let question = document.querySelectorAll(".question");
-
-question.forEach(question => {
-  question.addEventListener("click", event => {
-    const active = document.querySelector(".question.active");
-    if(active && active !== question ) {
-      active.classList.toggle("active");
-      active.nextElementSibling.style.maxHeight = 0;
-    }
-    question.classList.toggle("active");
-    const answer = question.nextElementSibling;
-    if(question.classList.contains("active")){
-      answer.style.maxHeight = answer.scrollHeight + "px";
-    } else {
-      answer.style.maxHeight = 0;
-    }
-  })
-})
-// FAQ JS End
+// Slider JS End
 
 
